24518c9d45c5e189abb2eae67257f4f70ba3f248
Atomiix <atomiix@localhost.localdomain>
2012-10-01 19:05:36 +0200

    ré-écriture complete du plugin
    >..." to unstage)

--- jquery.fix.js
+++ jquery.fix.js
@@ -1,31 +1,69 @@
 (function( $ ) {
-    $.fn.fix = function() {
+    $.fn.fix = function(options) {
 
+        var settings = $.extend({
+            'bottomLimit' : this.parent(),
+            'bottomMargin' : 0
+        }, options);
+        
         $this = this;
+        
+        this.parent().css('min-height' , this.height());
+        //save data
         var position = this.css("position");
-        var top = this.css("top");
-        var top = this.position().top;
-        var bottom = this.parent().height() + top - this.height();
-
+        var cssTop = this.css("top");
+        var cssLeft = this.css("left");
+        
+        //save real position
+        var realTop = this.offset().top;
+        var realLeft = this.offset().left;
+        var height = this.height();
+        var width = this.width();
+        
         $(window).scroll(function(){
-            console.log(bottom);
-            if($(window).scrollTop() >= top){
-                $this.css({
-                    "position": "fixed", 
-                    'top' : '0'
-                });
-                if($(window).scrollTop() > bottom){
+            bottomOffset = settings.bottomLimit.offset().top - settings.bottomMargin;
+            if(realTop < $(window).scrollTop()){
+                if(bottomOffset != null && $(window).scrollTop()+height > bottomOffset ){
                     $this.css({
-                        'position' : 'absolute', 
-                        'top' : bottom+"px"
-                        });
+                        'position' : 'relative',
+                        'top' : bottomOffset - height - realTop,
+                        'left' : 0
+                    });
+                }else{
+                    $this.css({
+                        'position' : 'fixed',
+                        'top' : '0',
+                        'left' : realLeft,
+                        'width' : width
+                    });
                 }
             }else{
                 $this.css({
-                    "position": position, 
-                    'top' : top
+                    'position' : position,
+                    'top' : cssTop,
+                    'left' : cssLeft
                 });
             }
         });
+        $(window).resize(function(){
+            
+            if($this.css('position') == 'fixed'){
+            $this.css({
+                'position' : position,
+                'top' : cssTop,
+                'left' : cssLeft
+            });
+            realLeft = $this.offset().left;
+            $this.css({
+                        'position' : 'fixed',
+                        'top' : '0',
+                        'left' : realLeft,
+                        'width' : width
+                    });
+            }else{
+                realLeft = $this.offset().left;
+            }
+        });
+        
     };
 })( jQuery );
